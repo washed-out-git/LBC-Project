767d08a7558e31724ad3f96aafe3d48ebe2ec0ed
Aaron Hoyt <aaron.hoyt@mykenzie.snhu.edu>
2023-03-23 18:31:40 -0500

    fixed buyer

--- Application/src/main/java/com/kenzie/appserver/service/model/Buyer.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Buyer.java
@@ -13,25 +13,12 @@ import static java.util.UUID.randomUUID;
 <<<<<<< HEAD
 public class Buyer extends User {
 
-    private final String userId;
+    private String userId;
     private String buyerName;
     public List<Bid> bidList;
 
     public Buyer(String buyerName) {
         this.buyerName = buyerName;
-=======
-public class Buyer {
-
-    private final String userId;
-    private String buyerName;
-    public List<String> bidList;
-
-    public Buyer(String sellerName) {
-        this.buyerName = sellerName;
->>>>>>> main
-        this.userId = randomUUID().toString();
-        this.bidList = new ArrayList<>();
-    }
 
     public String getUserId() {
         return userId;
@@ -45,19 +32,16 @@ public class Buyer {
         this.buyerName = buyerName;
     }
 
-<<<<<<< HEAD
     public List<Bid> getBidList() {
         return bidList;
     }
 
     public void setBidList(List<Bid> bidList) {
-=======
     public List<String> getBidList() {
         return bidList;
     }
 
     public void setBidList(List<String> bidList) {
->>>>>>> main
         this.bidList = bidList;
     }
 }
