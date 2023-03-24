e63253aa75083a7f5e2a0356f671cdd46ab47549
epentz88 <epentz88@gmail.com>
2023-03-24 12:58:44 -0500

    vehicle class updated

--- Application/src/main/java/com/kenzie/appserver/repositories/model/VehicleRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/VehicleRecord.java
@@ -2,7 +2,6 @@ package com.kenzie.appserver.repositories.model;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 import java.util.Objects;
