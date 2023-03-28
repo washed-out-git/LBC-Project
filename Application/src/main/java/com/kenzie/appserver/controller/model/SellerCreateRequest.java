28397adfbd81fb77e9532155fa21513c0207dfd4
tnbowen87 <tnbowen87@gmail.com>
2023-03-28 14:08:03 -0700

    Added seller repistory, controller and associated

---
+++ Application/src/main/java/com/kenzie/appserver/controller/model/SellerCreateRequest.java
@@ -0,0 +1,45 @@
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kenzie.appserver.service.model.Vehicle;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+public class SellerCreateRequest {
+    @NotEmpty
+    @JsonProperty("userId")
+    private String userId;
+
+    @NotEmpty
+    @JsonProperty("sellerName")
+    private String sellerName;
+
+    @NotEmpty
+    @JsonProperty("vehicleList")
+    public List<Vehicle> vehicleList;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public List<Vehicle> getVehicleList() {
+        return vehicleList;
+    }
+
+    public void setVehicleList(List<Vehicle> vehicleList) {
+        this.vehicleList = vehicleList;
+    }
+}
