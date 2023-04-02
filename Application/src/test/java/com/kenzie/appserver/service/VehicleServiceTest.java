020d7b09f14eb17b5b85c47e9c1b4110d347bf64
Aaron Hoyt <aaron.hoyt@mykenzie.snhu.edu>
2023-04-02 17:42:39 -0500

    rebasing

--- Application/src/test/java/com/kenzie/appserver/service/VehicleServiceTest.java
+++ Application/src/test/java/com/kenzie/appserver/service/VehicleServiceTest.java
@@ -71,6 +71,7 @@ public class VehicleServiceTest {
         String price = "20000";
         String sellerId = randomUUID().toString();
 
+
         Vehicle vehicle = new Vehicle(id, make, model, year, price, sellerId);
 
         ArgumentCaptor<VehicleRecord> vehicleRecordCaptor = ArgumentCaptor.forClass(VehicleRecord.class);
@@ -168,6 +169,7 @@ public class VehicleServiceTest {
         String sellerId = randomUUID().toString();
 
         Vehicle vehicle = new Vehicle(id, make, model, year, price, sellerId);
+
         VehicleRecord record = new VehicleRecord();
         record.setVehicleId(vehicle.getVehicleId());
         record.setMake(vehicle.getMake());
